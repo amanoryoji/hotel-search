0fc4b107c91269a5a1fc65fd94a64c25dd2dfc0a
amanoryoji <amanoryoji58@gmail.com>
2022-09-10 14:41:02 +0900

    css調整と背景画像のアップ

--- components/search.vue
+++ components/search.vue
@@ -1,38 +1,47 @@
 <template>
     <div class="search">
-        <div>
-            <label for="prefecture">地域</label>
-            <select id="pref" v-model="prefecture">
-                <option value="">都道府県を選択</option>
-                <option v-for="(item, index) in getPref" v-bind:value="item.no">
-                    {{ item.name }}
-                </option>
-            </select>
+        <div class="search-container">
+            <h3 class="top-title">あなたの旅をよりシンプルに。<br>全国各地の宿泊施設を検索しましょう！</h3>
 
-            <label for="city">市町村</label>
-            <select id="city" v-model="cities" v-bind:disabled="prefecture == ''">
-                <option value="">市町村を選択</option>
-                <option v-for="(item, index) in getCity" v-bind:value="item.no">
-                    {{ item.name }}
-                </option>
-            </select>
+            <div class="search-container__box">
+                <h4 class="search-container__box-title">条件から検索</h4>
+                <div class="search-input">
+                    <label for="prefecture">都道府県<span class="required">必須</span></label>
+                    <select id="pref" v-model="prefecture">
+                        <option value="">都道府県を選択</option>
+                        <option v-for="(item, index) in getPref" v-bind:value="item.no">
+                            {{ item.name }}
+                        </option>
+                    </select>
+                </div>
 
-            <span
-            v-if="cities == 'sapporo' || cities == 'tokyo'">
-                <label for="detailCity">地区</label>
-                <select id="detailCity" v-model="detailCity" v-bind:disabled="cities == ''">
-                    <option value="">地区を選択</option>
-                    <option v-for="(item, index) in getDetailCity" v-bind:value="item.no">
-                        {{ item.name }}
-                    </option>
-                </select>
-            </span>
+                <div class="search-input">
+                    <label for="city">市町村</label>
+                    <select id="city" v-model="cities" v-bind:disabled="prefecture == ''">
+                        <option value="">市町村を選択</option>
+                        <option v-for="(item, index) in getCity" v-bind:value="item.no">
+                            {{ item.name }}
+                        </option>
+                    </select>
+                </div>
 
-            <button v-on:click="click">検索する</button>
+                <div
+                v-if="cities == 'sapporo' || cities == 'tokyo'" class="search-input">
+                    <label for="detailCity">地区</label>
+                    <select id="detailCity" v-model="detailCity" v-bind:disabled="cities == ''">
+                        <option value="">地区を選択</option>
+                        <option v-for="(item, index) in getDetailCity" v-bind:value="item.no">
+                            {{ item.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <button v-on:click="click" class="search-button">検索する</button>
+            </div>
         </div>
 
-        <div v-if="hotels != ''">
-            <h3>検索件数：{{ hotels[0].data.hotels.length }}件</h3>
+        <div v-if="hotels != ''" class="search-container__result">
+            <h4 class="search-container__result-count">検索件数：<span>{{ hotels[0].data.hotels.length }}</span>件</h4>
 
             <no-ssr>
                 <paginate
@@ -42,6 +51,7 @@
                 :prev-text="'<'"
                 :next-text="'>'"
                 :hide-prev-next=true
+                :container-class="'pagination'"
                 ></paginate>
             </no-ssr> 
 
@@ -49,7 +59,7 @@
                 <li v-for="hotel in getHotels">
                     <p>{{ hotel.hotel[0].hotelBasicInfo.hotelName }} </p>
                     <img v-bind:src="hotel.hotel[0].hotelBasicInfo.hotelImageUrl">
-                    <p>最安料金{{ hotel.hotel[0].hotelBasicInfo.hotelMinCharge }}円</p>
+                    <p>1部屋1泊あたりの最安値：{{ hotel.hotel[0].hotelBasicInfo.hotelMinCharge }}円</p>
                     <nuxt-link v-bind:to="{path: `detail`, query: { name: encodeURIComponent(JSON.stringify(hotel))}}">
                         詳細を見る
                     </nuxt-link>
@@ -221,17 +231,120 @@ export default {
         height: 169px;
         object-fit: contain;
     }
+    li{
+        list-style: none;
+    }
     li.active{
         color: red;
     }
-    ul.hotelContents{
-        display: flex;
-        flex-wrap: wrap;
-        width: 1000px;
-        margin-top:30px;
 
-        li{
-            width: 33%;
+    button{
+        border: none;
+        cursor: pointer;
+        outline: none;
+        padding: 0;
+        appearance: none;
+        background: linear-gradient(135deg,#c00,#ec64a9);
+        padding: 12px 8px;
+        display: block;
+        width: 100%;
+        margin: 0 auto;
+        color: #fff;
+        font-size: 1.6rem;
+        font-weight: 700;
+        text-align: center;
+        transition: 0.2s;
+
+        &:hover{
+            border-radius: 48px;
+            overflow: hidden;
+        }
+    }
+
+    .search{
+
+        &-container{
+            background-image: url(/images/search-bg.jpg);
+            background-size: 100% auto;
+            padding: 80px 0;
+
+            .top-title{
+                margin-bottom: 50px;
+                text-align: center;
+                color: #fff;
+                text-shadow: 0 2px 6px #333;
+                font-size: 40px;
+            }
+
+            &__box{
+                background: rgba(255,255,255,.8);
+                width: 700px;
+                padding: 50px;
+                margin: 0 auto;
+                box-sizing: border-box;
+
+                &-title{
+                    margin-bottom: 40px;
+                }
+
+                .search-input{
+                    margin-bottom: 40px;
+
+                    label{
+                        display: block;
+                        margin-bottom: 20px;
+                    }
+
+                    select{
+                        width: 100%;
+                        padding: 10px 0;
+                    }
+                }
+
+                button{
+                    width: 60%;
+                }
+            }
+
+            &__result{
+                background-color: rgba(21,23,33,.05);
+                padding: 40px 0;
+
+                &-count{
+                    margin-bottom: 40px;
+                    text-align: center;
+                }
+
+                .pagination{
+                    display: flex;
+                    justify-content: center;
+                    margin-bottom: 40px;
+
+                    li{
+                        margin-right: 30px;
+
+                        &:last-of-type{
+                            margin-right: 0;
+                        }
+                    }
+                }
+                
+                ul.hotelContents{
+                    display: flex;
+                    flex-wrap: wrap;
+                    width: 90%;
+                    margin: 0 auto;
+
+                    li{
+                        width: 33.3%;
+                        margin-bottom: 30px;
+                        
+                        &:last-of-type{
+                            margin-bottom: 0;
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
